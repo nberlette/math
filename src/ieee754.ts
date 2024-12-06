372af38facfcee910be4bc96ae3aaffb2656bf64
Nicholas Berlette <nick@berlette.com>
2024-12-06 09:02:15 -0800

    feat(ieee754): add ieee754 entrypoint

---
+++ src/ieee754.ts
@@ -0,0 +1,15 @@
+/**
+ * IEEE 754 floating point number manipulation.
+ *
+ * This module provides functions for encoding, decoding, and rounding floating
+ * point numbers according to the IEEE 754 standard, for both 16-bit and 32-bit
+ * precision. For utilities specific to 16-bit (half-precision) floats, see the
+ * `float16` module. For 32-bit (single-precision), see the `float32` module.
+ *
+ * **Note**: The `float16` and `float32` modules are used under the hood by the
+ * `f16round` and `fround` functions, respectively.
+ *
+ * @module ieee754
+ */
+export * from "./float16/mod.ts";
+export * from "./float32/mod.ts";
