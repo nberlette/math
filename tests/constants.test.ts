b4db149f551e9f3089479b52e467d251f27a0b54
Nicholas Berlette <nick@berlette.com>
2024-12-09 11:01:16 -0800

    tests(constants): add tests for constants

---
+++ tests/constants.test.ts
@@ -0,0 +1,40 @@
+import * as constants from "../src/constants/mod.ts";
+import { describe, it } from "jsr:@std/testing@1/bdd";
+import { expect } from "jsr:@std/expect@1";
+
+const cases = {
+  Math: [
+    "E",
+    "LN2",
+    "LN10",
+    "LOG2E",
+    "LOG10E",
+    "PI",
+    "SQRT1_2",
+    "SQRT2",
+  ],
+  Number: [
+    "EPSILON",
+    "MAX_SAFE_INTEGER",
+    "MIN_SAFE_INTEGER",
+    "NEGATIVE_INFINITY",
+    "POSITIVE_INFINITY",
+    "NaN",
+    "MAX_VALUE",
+    "MIN_VALUE",
+  ],
+} as const;
+
+for (const kind of Object.keys(cases) as (keyof typeof cases)[]) {
+  const keys = cases[kind];
+  for (const key of keys) {
+    describe(key, () => {
+      const value = constants[key];
+      const parent = globalThis[kind];
+      const expected = parent[key as keyof typeof parent];
+      it(`is equal to ${kind}.${key}`, () => {
+        expect(value).toBe(expected);
+      });
+    });
+  }
+}
