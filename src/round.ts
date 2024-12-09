86daa739daf7d32ded4f083225ab8810e8d2fabb
Nicholas Berlette <nick@berlette.com>
2024-12-09 04:32:43 -0800

    docs(round): improve docs and categorization

--- src/round.ts
+++ src/round.ts
@@ -1,16 +1,15 @@
 /**
- * Performant local implementation of the `Math.round` function.
+ * Rounds a number to the nearest whole number.
  *
- * @category Arithmetic
  * @module round
  */
 
 /**
- * Performant local implementation of the `Math.round` function.
+ * Rounds a number to the nearest whole number.
  *
  * @param x The number to round to the nearest whole number
  * @returns The nearest whole number to the provided number.
- * @category Arithmetic
+ * @category Rounding
  */
 export function round(x: number): number {
   return (x = +x) < 0 ? -round(-x) : (
