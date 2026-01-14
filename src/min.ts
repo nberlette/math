fb9e145e4280efedbb680f157cbc150ee10823eb
Nicholas Berlette <nick@berlette.com>
2026-01-13 18:58:05 -0800

    fix(min): ensure min behaves like Math.min

--- src/min.ts
+++ src/min.ts
@@ -5,6 +5,7 @@
  * @module min
  */
 import { POSITIVE_INFINITY } from "./constants/positive_infinity.ts";
+import { isNegativeZero } from "./guards/negative_zero.ts";
 
 /**
  * Returns the smallest of its arguments, or `NaN` if any argument is `NaN`.
@@ -19,7 +20,13 @@ export function min(...numbers: number[]): number {
   for (let i = 0; i < numbers.length; i++) {
     const number = numbers[i];
     if (number !== number) return number;
-    if (number < result) result = number;
+    if (number < result) {
+      result = number;
+      continue;
+    }
+    if (number === 0 && result === 0 && isNegativeZero(number)) {
+      result = number;
+    }
   }
   return result;
 }
