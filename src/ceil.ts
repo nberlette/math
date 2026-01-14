c3c3c273205d047b196f4b4873f02759f7ea461d
Nicholas Berlette <nick@berlette.com>
2026-01-13 18:57:59 -0800

    fix(ceil): ensure ceil behaves like Math.ceil

--- src/ceil.ts
+++ src/ceil.ts
@@ -4,7 +4,8 @@
  * @module ceil
  */
 
-import { floor } from "./floor.ts";
+import { NEGATIVE_INFINITY } from "./constants/negative_infinity.ts";
+import { POSITIVE_INFINITY } from "./constants/positive_infinity.ts";
 
 /**
  * Performant local implementation of the `Math.ceil` function.
@@ -15,5 +16,12 @@ import { floor } from "./floor.ts";
  * @tags rounding, ceiling
  */
 export function ceil(x: number): number {
-  return (x = +x) < 0 ? -floor(-x) : x % 1 ? x - x % 1 + 1 : x;
+  x = +x;
+  if (x !== x || x === POSITIVE_INFINITY || x === NEGATIVE_INFINITY) return x;
+  if (x === 0) return x;
+  const remainder = x % 1;
+  if (remainder === 0) return x;
+  if (x < 0 && x > -1) return -0;
+  if (x > 0) return x - remainder + 1;
+  return x - remainder;
 }
